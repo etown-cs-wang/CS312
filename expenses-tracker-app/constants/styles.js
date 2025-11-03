4be563c388bf7b01b2291ab3193e90f27801808a
Jingwen Wang <jingwen.w2010@gmail.com>
2025-11-03 11:19:44 -0500

    expenses tracker app

---
+++ expenses-tracker-app/constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '#ede9f8ff',
+    primary100: '#d0c0f6ff',
+    primary200: '#f1edf9ff',
+    primary400: '#7961b0ff',
+    primary500: '#4d2f95ff',
+    primary700: '#2f1865f6',
+    primary800: '#976ff4ff',
+    accent500: '#fae6cdff',
+    error50: '#fcc4e4',
+    error500: '#9b095c',
+    gray500: '#39324a',
+    gray700: '#221c30',
+  },
+};
